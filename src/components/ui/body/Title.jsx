031a2ee9ad5b2d61529c8a50a283a02553d6a295
Alessio S. <157212548+xScafu@users.noreply.github.com>
2025-01-16 08:14:56 +0100

    Update Title.jsx

--- src/components/ui/body/Title.jsx
+++ src/components/ui/body/Title.jsx
@@ -2,10 +2,10 @@ export default function Title() {
   return (
     <>
       <div className="absolute z-10 left-5 py-2 px-4 bg-apricot dark:bg-darkPurple">
-        <h1 className="text-bistre dark:text-englishViolet text-4xl font-bold dark:font-extrabold uppercase">
+        <h1 className="text-bistre text-4xl font-bold dark:font-extrabold uppercase">
           Ma che ne sai <br /> di me
         </h1>
-        <h3 className="text-bistre dark:text-englishViolet text-2xl text-end font-bold mt-2">
+        <h3 className="text-bistre text-2xl text-end font-bold mt-2">
           Nuovo singolo
         </h3>
       </div>
