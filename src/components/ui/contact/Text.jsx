59c4e5b5e7219bcd9a5f241da997d950a4481793
xScafu <a.scarlata93@gmail.com>
2025-03-07 14:29:56 +0100

    contact text

---
+++ src/components/ui/contact/Text.jsx
@@ -0,0 +1,9 @@
+export default function Text() {
+  return (
+    <>
+      <div className="text-center font-serif text-2xl text-bistre dark:text-wine">
+        <p>TESTO DI PROVA</p>
+      </div>
+    </>
+  );
+}
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,11 +1,14 @@
 import Footer from "../components/Footer";
 import Form from "../components/ui/contact/Form";
-import UnderConstruction from "../components/UnderCostruction";
+import Text from "../components/ui/contact/Text";
 
 export default function About() {
   return (
     <>
-      <div className="my-96">
+      <div className="my-36">
+        <Text />
+      </div>
+      <div className="my-36">
         <Form />
       </div>
       <Footer />
