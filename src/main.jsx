5a75c5840f9687c3fb4fd71d7c7051f960558a6b
xScafu <a.scarlata93@gmail.com>
2025-04-19 17:00:23 +0200

    analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import App from "./App.jsx";
 import store from "./app/store.js";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
+import { Analytics } from "@vercel/analytics/react";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <div className="relative">
+          <Analytics />
           <App />
         </div>
       </Provider>
